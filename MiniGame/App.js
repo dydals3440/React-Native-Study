d8a0c18ecc6a8d7f2c9897d3c069530f4e71722f
김용민 <dydals3440@gmail.com>
2024-01-15 19:11:30 +0900

    feat: flex: 1 속성으로 앱 전체에 색상 넣기

--- MiniGame/App.js
+++ MiniGame/App.js
@@ -4,7 +4,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    // View는 콘텐츠가 들어가는 높이만 차지
+    <View style={styles.rootScreen}>
+      <StartGameScreen />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+    backgroundColor: '#ddb52f',
+  },
+});
