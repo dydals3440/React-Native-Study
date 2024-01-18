b985199b8cf22ada2940c00bf901e645f8fa45c0
김용민 <dydals3440@gmail.com>
2024-01-18 19:13:20 +0900

    feat: +, - logic 추가

--- MiniGame/screens/GameScreen.jsx
+++ MiniGame/screens/GameScreen.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { Text, View, StyleSheet, SafeAreaView } from 'react-native';
+import { Text, View, StyleSheet, SafeAreaView, Alert } from 'react-native';
 import Title from '../components/ui/Title';
 import NumberContainer from '../components/game/NumberContainer';
+import PrimaryButton from '../components/ui/PrimaryButton';
 
 // 최신 스마트폰에는 노치가 있습니다 .콘텐츠가 노치에 너무 가깝거나, 가려지지 않도록 거리를 두어야 한다. 노치가 없는 스마트폰도 있고, 스마트폰의 크기도 제각각이다. 실행중인 장치를 자동으로 감지하고, 노치와 상태 표시줄과 콘텐츠 사이에 적절한 간격을 자동으로 추가하는 컴포넌트가 SafeAreaView이다.
 
@@ -16,16 +17,58 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
+// 변수를 바깥에 선언하면, 컴포넌트가 재평가할떄 변수가 달라지지 않는다.
+let minBoundary = 1;
+let maxBoundary = 100;
+
 const GameScreen = ({ userNumber }) => {
-  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const initialGuess = generateRandomBetween(
+    minBoundary,
+    maxBoundary,
+    userNumber
+  );
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  const nextGuessHandler = (direction) => {
+    // 같은 수일떄 에러 발생
+    if (
+      (direction === 'lower' && currentGuess < userNumber) ||
+      (direction === 'greater' && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie", 'You know that this is wrong...', [
+        { text: 'Sorry!', style: 'cancel' },
+      ]);
+      return;
+    }
+
+    if (direction === 'lower') {
+      maxBoundary = currentGuess;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+    console.log(minBoundary, maxBoundary);
+    const newRndNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+    setCurrentGuess(newRndNumber);
+  };
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
       <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or lower?</Text>
-        {/* + - */}
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+            +
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+            -
+          </PrimaryButton>
+        </View>
       </View>
       <Text>LOG ROUNDS</Text>
     </View>
