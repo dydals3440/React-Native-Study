21e8ea4033051ba72c049258b4c4250193746f19
김용민 <dydals3440@gmail.com>
2024-01-21 19:41:30 +0900

    feat: Icon으로 작업하기

--- MiniGame/screens/GameScreen.jsx
+++ MiniGame/screens/GameScreen.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Text, View, StyleSheet, Alert } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
 import Title from '../components/ui/Title';
 import NumberContainer from '../components/game/NumberContainer';
 import PrimaryButton from '../components/ui/PrimaryButton';
@@ -72,12 +74,12 @@ const GameScreen = ({ userNumber, onGameOver }) => {
         <View style={styles.buttonsContainer}>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
-              +
+              <Ionicons name='md-remove' size={24} color='white' />
             </PrimaryButton>
           </View>
           <View style={styles.buttonContainer}>
             <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
-              -
+              <Ionicons name='md-add' size={24} color='white' />
             </PrimaryButton>
           </View>
         </View>
