ab5a251a6308397e16a95ffb6d5afa3a24f3d0a2
김용민 <dydals3440@gmail.com>
2024-01-29 20:25:13 +0900

    feat: useWindowDimensions 훅 사용

--- MiniGame/screens/StartGameScreen.js
+++ MiniGame/screens/StartGameScreen.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
-import { TextInput, View, StyleSheet, Alert } from 'react-native';
+import {
+  TextInput,
+  View,
+  StyleSheet,
+  Alert,
+  Dimensions,
+  useWindowDimensions,
+} from 'react-native';
 
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
@@ -9,6 +16,11 @@ import InstructionText from '../components/ui/InstructionText';
 
 function StartGameScreen({ onPickNumber }) {
   const [enteredNumber, setEnteredNumber] = useState('');
+
+  // windowDimensions를 쓰는게 좋음 (dimensions는 스타일코드에 딱 한번 앱이 켜질떄 실행됨. 여기다가 쓰면 매번 재실행되기 떄문이다(함수컴포넌트))
+  const { width, height } = useWindowDimensions();
+
+  const marginTopDistance = height < 380 ? 30 : 100;
 
   function numberInputHandler(enteredText) {
     setEnteredNumber(enteredText);
@@ -34,17 +46,16 @@ function StartGameScreen({ onPickNumber }) {
   }
 
   return (
-    <View style={styles.rootContainer}>
+    // 이 컴포넌트 함수는 치수가 변경될떄마다 재실행됨 (그래서 Dimensions가 아닌 useWindowDimensions 훅을 사용하는 것)
+    <View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
       <Title>Guess My Number</Title>
       <Card>
-        <InstructionText>
-          Enter a Number
-        </InstructionText>
+        <InstructionText>Enter a Number</InstructionText>
         <TextInput
           style={styles.numberInput}
           maxLength={2}
-          keyboardType="number-pad"
-          autoCapitalize="none"
+          keyboardType='number-pad'
+          autoCapitalize='none'
           autoCorrect={false}
           onChangeText={numberInputHandler}
           value={enteredNumber}
@@ -64,10 +75,12 @@ function StartGameScreen({ onPickNumber }) {
 
 export default StartGameScreen;
 
+// const deviceHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
   rootContainer: {
     flex: 1,
-    marginTop: 100,
+    // marginTop: deviceHeight < 450 ? 30 : 100,
     alignItems: 'center',
   },
   numberInput: {
