de52214f94827e3e31b6a299aaf0e8543a195874
김용민 <dydals3440@gmail.com>
2024-01-29 20:09:19 +0900

    style: dimension API를 통해, 다양한 기기에 동적 스타일링 적용

--- MiniGame/components/game/NumberContainer.js
+++ MiniGame/components/game/NumberContainer.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
 
 import Colors from '../../constants/colors';
 
@@ -12,20 +12,24 @@ function NumberContainer({ children }) {
 
 export default NumberContainer;
 
+// ios에서 screen, window 차이없음.
+// android에서 screen은 상태표시줄을 포함한 너비와 높이, window는 상태 표시줄을 제외한 너비와 높이이다.
+const deviceWidth = Dimensions.get('window').width;
+
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: 24,
-    margin: 24,
+    padding: deviceWidth < 380 ? 12 : 24,
+    margin: deviceWidth < 380 ? 12 : 24,
     borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
   },
   numberText: {
     color: Colors.accent500,
-    fontSize: 36,
+    fontSize: deviceWidth < 380 ? 12 : 36,
     // fontWeight: 'bold',
-    fontFamily: 'open-sans-bold'
+    fontFamily: 'open-sans-bold',
   },
 });
