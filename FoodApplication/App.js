65c34e1d4f6e4532a16298838075a0c7b4cb8a2d
김용민 <dydals3440@gmail.com>
2024-01-31 20:11:22 +0900

    feat: grid item display AOS/IOS 구분 스타일링

--- FoodApplication/App.js
+++ FoodApplication/App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StatusBar, StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <ScrollView>
+      <StatusBar style='light' />
       <CategoriesScreen />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
--- FoodApplication/screens/CategoriesScreen.jsx
+++ FoodApplication/screens/CategoriesScreen.jsx
@@ -14,6 +14,8 @@ const CategoriesScreen = () => {
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
       renderItem={renderCategoryItem}
+      // 기본 1열 -> 2열로 변경
+      numColumns={2}
     />
   );
 };
