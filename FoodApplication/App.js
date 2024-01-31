2a12e1472c0a9a01c66fff24a202ed3cebede49d
김용민 <dydals3440@gmail.com>
2024-01-31 20:27:15 +0900

    feat: react-navigation 패키지 사용

--- FoodApplication/App.js
+++ FoodApplication/App.js
@@ -1,12 +1,21 @@
-import { ScrollView, StatusBar, StyleSheet } from 'react-native';
+import { StatusBar, StyleSheet } from 'react-native';
 import CategoriesScreen from './screens/CategoriesScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <ScrollView>
-      <StatusBar style='light' />
-      <CategoriesScreen />
-    </ScrollView>
+    <>
+      <StatusBar style='dark' />
+      <NavigationContainer>
+        {/* 멋진 헤더 추가, 충돌하지 않도록 안전영역도 추가 */}
+        <Stack.Navigator>
+          <Stack.Screen name='MealsCategories' component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
