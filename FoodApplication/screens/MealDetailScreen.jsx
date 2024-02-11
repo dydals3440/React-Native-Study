24f2f22f19506d63625597057f1bf270bc5bf143
김용민 <dydals3440@gmail.com>
2024-02-11 19:37:18 +0900

    feat: 음식 상세 화면에 콘텐츠 출력하기

--- FoodApplication/screens/MealDetailScreen.jsx
+++ FoodApplication/screens/MealDetailScreen.jsx
@@ -1,8 +1,30 @@
-import { Text } from 'react-native';
+import { Image, Text, View } from 'react-native';
+import { MEALS } from '../data/dummy-data';
+import MealDetails from '../components/MealDetails';
 
 const MealDetailScreen = ({ route, navigation }) => {
   const mealId = route.params.mealId;
-  return <Text>This is the Meal Detail Screen (${mealId})</Text>;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  return (
+    <View>
+      <Image source={{ uri: selectedMeal.imageUrl }} />
+      <Text>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+      />
+      <Text>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient) => (
+        <Text key={ingredient}>{ingredient}</Text>
+      ))}
+      <Text>
+        {selectedMeal.steps.map((step) => (
+          <Text key={step}>{step}</Text>
+        ))}
+      </Text>
+    </View>
+  );
 };
 
 export default MealDetailScreen;
