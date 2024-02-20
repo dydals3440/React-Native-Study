c2595f009022bf4475e628676cc4755211846ad0
김용민 <dydals3440@gmail.com>
2024-02-21 00:20:24 +0900

    feat: 하단 탭 사용 방법 정리

--- DrawerNavigator/App.js
+++ DrawerNavigator/App.js
@@ -1,8 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
 import { Ionicons } from '@expo/vector-icons';
+
+const BottomTab = createBottomTabNavigator();
 
 const Drawer = createDrawerNavigator();
 
@@ -10,36 +13,32 @@ export default function App() {
   return (
     <NavigationContainer>
       {/* 개별속성줘도되지만, 공통속성 줘도됨 */}
-      <Drawer.Navigator
+      <BottomTab.Navigator
         screenOptions={{
           headerStyle: { backgroundColor: '#3c0a6b' },
           headerTintColor: 'white',
-          drawerActiveBackgroundColor: '#f0e1ff',
-          drawerActiveTintColor: '#3c0a6b',
-          drawerStyle: { backgroundColor: '#ccc' },
+          tabBarActiveTintColor: '#3c0a6b',
         }}
       >
-        <Drawer.Screen
+        <BottomTab.Screen
           name='Welcome'
           component={WelcomeScreen}
           options={{
-            drawerLabel: 'Welcome Screen',
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name='home' color={color} size={size} />
             ),
           }}
         />
-        <Drawer.Screen
+        <BottomTab.Screen
           name='User'
           component={UserScreen}
           options={{
-            drawerLabel: 'Welcome Screen',
-            drawerIcon: ({ color, size }) => (
+            tabBarIcon: ({ color, size }) => (
               <Ionicons name='person' color={color} size={size} />
             ),
           }}
         />
-      </Drawer.Navigator>
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
