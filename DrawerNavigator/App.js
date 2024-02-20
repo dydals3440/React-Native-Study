5a9122bde3304b81d807c662834dffd0de341fd9
김용민 <dydals3440@gmail.com>
2024-02-20 23:47:54 +0900

    feat: Drawere navigation와 Drawere 구성하기

--- DrawerNavigator/App.js
+++ DrawerNavigator/App.js
@@ -2,15 +2,43 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import WelcomeScreen from './screens/WelcomeScreen';
 import UserScreen from './screens/UserScreen';
+import { Ionicons } from '@expo/vector-icons';
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName='User'>
-        <Drawer.Screen name='Welcome' component={WelcomeScreen} />
-        <Drawer.Screen name='User' component={UserScreen} />
+      {/* 개별속성줘도되지만, 공통속성 줘도됨 */}
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: '#3c0a6b' },
+          headerTintColor: 'white',
+          drawerActiveBackgroundColor: '#f0e1ff',
+          drawerActiveTintColor: '#3c0a6b',
+          drawerStyle: { backgroundColor: '#ccc' },
+        }}
+      >
+        <Drawer.Screen
+          name='Welcome'
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name='home' color={color} size={size} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name='User'
+          component={UserScreen}
+          options={{
+            drawerLabel: 'Welcome Screen',
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name='person' color={color} size={size} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
--- DrawerNavigator/screens/UserScreen.js
+++ DrawerNavigator/screens/UserScreen.js
@@ -1,11 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-const UserScreen = () => {
+const UserScreen = ({ route, navigation }) => {
+  function openDrawerHandler() {
+    // route / navigation props
+    navigation.toggleDrawer();
+  }
   return (
     <View style={styles.rootContainer}>
       <Text>
         This is the <Text style={styles.highlight}>"User"</Text> screen!
       </Text>
+      <Button title='Open Drawer' onPress={openDrawerHandler} />
     </View>
   );
 };
