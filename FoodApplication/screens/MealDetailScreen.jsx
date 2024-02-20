983a703543e48cc5d11bf23c597e2b625cad54c9
김용민 <dydals3440@gmail.com>
2024-02-20 23:00:59 +0900

    feat: 헤더에 아이콘 버튼 추가하기

--- FoodApplication/screens/MealDetailScreen.jsx
+++ FoodApplication/screens/MealDetailScreen.jsx
@@ -11,6 +11,7 @@ import MealDetails from '../components/MealDetails';
 import SubTitle from '../components/MealDetail/SubTitle';
 import List from '../components/MealDetail/List';
 import { useLayoutEffect } from 'react';
+import IconButton from '../components/IconButton';
 
 const MealDetailScreen = ({ route, navigation }) => {
   const mealId = route.params.mealId;
@@ -23,7 +24,13 @@ const MealDetailScreen = ({ route, navigation }) => {
   useLayoutEffect(() => {
     navigation.setOptions({
       headerRight: () => {
-        return <Button title='Tap me' onPress={headerButtonPressHandler} />;
+        return (
+          <IconButton
+            onPress={headerButtonPressHandler}
+            icon='star'
+            color='white'
+          />
+        );
       },
     });
   }, [navigation, headerButtonPressHandler]);
