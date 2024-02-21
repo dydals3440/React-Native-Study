dbda3c043aef2a0ecf8378434d891328f9e5b57e
김용민 <dydals3440@gmail.com>
2024-02-21 11:58:00 +0900

    feat: Context로 앱 전반에 걸친 상태 관리하기

--- FoodApplication/store/favorites-context.js
+++ FoodApplication/store/favorites-context.js
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 // 컨텍스트는 2개의 value를 전달.
 export const FavoritesContext = createContext({
@@ -8,7 +8,29 @@ export const FavoritesContext = createContext({
 });
 
 const FavoritesContextProvider = ({ children }) => {
-  return <FavoritesContext.Provider>{children}</FavoritesContext.Provider>;
+  const [favoriteMealIds, setFavoriteMealIds] = useState();
+  const addFavorite = (id) => {
+    // 상태 업데이트함수로 어떤 함수를 전달하면, 그 함수가 이전 상태 스냅샷을 수신
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  };
+  const removeFavorite = (id) => {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  };
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite: addFavorite,
+    removeFavorite: removeFavorite,
+  };
+  return (
+    // 이 value는 객체인 value 상수를 참조,
+    // ㅇ뤼는 이 객체를 값으로 콘텍스트 제공자에게 전달.
+    <FavoritesContext.Provider value={value}>
+      {children}
+    </FavoritesContext.Provider>
+  );
 };
 
 export default FavoritesContextProvider;
