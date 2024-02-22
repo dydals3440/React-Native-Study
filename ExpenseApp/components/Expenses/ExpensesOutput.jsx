9509efb81def91f7e00c7d995a80d406f75ca33e
김용민 <dydals3440@gmail.com>
2024-02-22 20:07:33 +0900

    feat: ExpenseItem 컴포넌트 제작

--- ExpenseApp/components/Expenses/ExpensesOutput.jsx
+++ ExpenseApp/components/Expenses/ExpensesOutput.jsx
@@ -1,8 +1,39 @@
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import ExpensesSummary from './ExpensesSummary';
 import ExpensesList from './ExpensesList';
+import { GlobalStyles } from '../../constants/styles';
 
 const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    description: 'A pair of shoes',
+    amount: 59.99,
+    date: new Date('2023-02-22'),
+  },
+  {
+    id: 'e2',
+    description: 'A pair of trousers',
+    amount: 90.29,
+    date: new Date('2023-03-25'),
+  },
+  {
+    id: 'e3',
+    description: 'Some Bananas',
+    amount: 5.29,
+    date: new Date('2023-03-29'),
+  },
+  {
+    id: 'e4',
+    description: 'A book',
+    amount: 14.99,
+    date: new Date('2022-02-19'),
+  },
+  {
+    id: 'e5',
+    description: 'Another book',
+    amount: 24.99,
+    date: new Date('2024-02-22'),
+  },
   {
     id: 'e1',
     description: 'A pair of shoes',
@@ -37,7 +68,7 @@ const DUMMY_EXPENSES = [
 
 const ExpensesOutput = ({ expenses, expensesPeriod }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
       <ExpensesList expenses={DUMMY_EXPENSES} />
     </View>
@@ -46,4 +77,12 @@ const ExpensesOutput = ({ expenses, expensesPeriod }) => {
 
 export default ExpensesOutput;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 24,
+    paddingTop: 24,
+    paddingBottom: 0,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
