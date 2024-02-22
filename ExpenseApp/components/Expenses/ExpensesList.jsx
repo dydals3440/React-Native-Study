18350bb3d8f4fcf49d279dd4a120a39da4e68110
김용민 <dydals3440@gmail.com>
2024-02-22 18:59:46 +0900

    feat: ExpenseList FlatList

--- ExpenseApp/components/Expenses/ExpensesList.jsx
+++ ExpenseApp/components/Expenses/ExpensesList.jsx
@@ -1,8 +1,17 @@
-import { StyleSheet, FlatList } from 'react-native';
-import React from 'react';
+import { StyleSheet, FlatList, Text } from 'react-native';
 
-const ExpensesList = () => {
-  return <FlatList />;
+const renderExpenseItem = (itemData) => {
+  return <Text>{itemData.item.description}</Text>;
+};
+
+const ExpensesList = ({ expenses }) => {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpenseItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
 };
 
 export default ExpensesList;
