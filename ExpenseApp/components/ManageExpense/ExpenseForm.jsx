75bfdd156878685f73438ecbf7cb198b02d754d0
김용민 <dydals3440@gmail.com>
2024-02-23 11:15:53 +0900

    feat: 양식 레이아웃 설정

--- ExpenseApp/components/ManageExpense/ExpenseForm.jsx
+++ ExpenseApp/components/ManageExpense/ExpenseForm.jsx
@@ -4,22 +4,28 @@ import Input from './Input';
 const ExpenseForm = () => {
   const amountChangeHandler = () => {};
   return (
-    <View>
-      <Input
-        label='Amount'
-        textInputConfig={{
-          keyboardType: 'decimal-pad',
-          onChangeText: amountChangeHandler,
-        }}
-      />
-      <Input
-        label='Date'
-        textInputConfig={{
-          placeholder: 'YYYY-MM-DD',
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputRow}>
+        {/*  style={styles.rowInput}를 통해, 아래 input에는 영향 안가게 */}
+        <Input
+          style={styles.rowInput}
+          label='Amount'
+          textInputConfig={{
+            keyboardType: 'decimal-pad',
+            onChangeText: amountChangeHandler,
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label='Date'
+          textInputConfig={{
+            placeholder: 'YYYY-MM-DD',
+            maxLength: 10,
+            onChangeText: () => {},
+          }}
+        />
+      </View>
       <Input
         label='Description'
         textInputConfig={{
@@ -34,4 +40,22 @@ const ExpenseForm = () => {
 
 export default ExpenseForm;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    marginTop: 40,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+    marginVertical: 24,
+    textAlign: 'center',
+  },
+  inputRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
