31ec45e8336933cf401011211c05f21acd567ec0
김용민 <dydals3440@gmail.com>
2024-03-01 01:18:55 +0900

    feat: 선택된 장소 확인하기

--- Native-Device/screens/Map.jsx
+++ Native-Device/screens/Map.jsx
@@ -1,8 +1,10 @@
-import { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useLayoutEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import IconButton from '../components/UI/IconButton';
+import { useCallback } from 'react';
 
-const Map = () => {
+const Map = ({ navigation }) => {
   const [selectedLocation, setSelectedLocation] = useState();
 
   const region = {
@@ -19,6 +21,35 @@ const Map = () => {
 
     setSelectedLocation({ lat: lat, lng: lng });
   };
+
+  // 무한 렌더를 방지하기위해 UseCallback으로 감싸, 불필요한 리렌더링 사이클과, 무한렌더링을 방지
+  const savePickedLocationHandler = useCallback(() => {
+    if (!selectedLocation) {
+      Alert.alert(
+        'No location picked!',
+        'You have to pick a location (by tapping on the map) first!'
+      );
+      return;
+    }
+    navigation.navigate('AddPlace', {
+      pickedLat: selectedLocation.lat,
+      pickedLng: selectedLocation.lng,
+    });
+  }, [navigation, selectedLocation]);
+
+  // 헤더 설정
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: ({ tintColor }) => (
+        <IconButton
+          icon='save'
+          size={24}
+          color={tintColor}
+          onPress={savePickedLocationHandler}
+        />
+      ),
+    });
+  }, [navigation, savePickedLocationHandler]);
 
   return (
     <MapView
