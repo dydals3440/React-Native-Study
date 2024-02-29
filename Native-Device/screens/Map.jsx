228f77a1cfa05cbd3b7220fff0ce6d89220d84ef
김용민 <dydals3440@gmail.com>
2024-03-01 01:07:14 +0900

    feat: 지도 상호작용 허용하기 & 마커 추가하기

--- Native-Device/screens/Map.jsx
+++ Native-Device/screens/Map.jsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 
 const Map = () => {
+  const [selectedLocation, setSelectedLocation] = useState();
+
   const region = {
     latitude: 37.550165,
     longitude: 127.12752,
@@ -9,7 +12,31 @@ const Map = () => {
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+
+  const selectLocationHandler = (event) => {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: lat, lng: lng });
+  };
+
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title='Picked Location'
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 };
 
 export default Map;
