e205ceaa9e74a35b8d94b46290efbbd3d0115e48
김용민 <dydals3440@gmail.com>
2024-02-29 22:52:21 +0900

    feat: LocationPicker 컴포넌트 제작

--- Native-Device/components/Places/PlaceForm.jsx
+++ Native-Device/components/Places/PlaceForm.jsx
@@ -2,6 +2,7 @@ import { ScrollView, StyleSheet, Text, View, TextInput } from 'react-native';
 import React, { useState } from 'react';
 import { Colors } from '../../constants/colors';
 import ImagePicker from './ImagePicker';
+import LocationPicker from './LocationPicker';
 
 const PlaceForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
@@ -20,6 +21,7 @@ const PlaceForm = () => {
         />
       </View>
       <ImagePicker />
+      <LocationPicker />
     </ScrollView>
   );
 };
